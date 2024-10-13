d2a8cf5761867dbdfea421d7ad490f73ec96aa50
CrackedMatter <81803926+CrackedMatter@users.noreply.github.com>
2024-10-13 17:29:34 +0200

    Add .clang-format

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,8 @@
 struct ResourceLocation {
     int32_t     mFileSystem = 0;
     std::string mPath;
-    uint64_t    mPathHash   = 0;
-    uint64_t    mFullHash   = 0;
+    uint64_t    mPathHash = 0;
+    uint64_t    mFullHash = 0;
 
     explicit ResourceLocation(const std::string& path) { mPath = path; }
 };
@@ -45,36 +45,38 @@ std::string AppPlatform_readAssetFile(void* self, std::string& filename) {
     return AppPlatform_readAssetFile_hook.call<std::string>(self, filename);
 }
 
-extern "C" __attribute__ ((visibility ("default"))) void mod_preinit() {}
+extern "C" __attribute__((visibility("default"))) void mod_preinit() {}
 
-extern "C" __attribute__ ((visibility ("default"))) void mod_init() {
+extern "C" __attribute__((visibility("default"))) void mod_init() {
     using namespace hat::literals::signature_literals;
 
     static std::span<std::byte> r;
 
-    dl_iterate_phdr([](dl_phdr_info* info, size_t, void* mc) {
-        auto h = dlopen(info->dlpi_name, RTLD_NOLOAD);
-        dlclose(h);
-        if (h == mc) {
-            for (auto& phdr: std::span{info->dlpi_phdr, info->dlpi_phnum}) {
-                if (phdr.p_type == PT_LOAD && phdr.p_flags & PF_X) {
-                    r = {reinterpret_cast<std::byte*>(info->dlpi_addr + phdr.p_vaddr), phdr.p_memsz};
-                    return 1;
+    dl_iterate_phdr(
+        [](dl_phdr_info* info, size_t, void* mc) {
+            auto h = dlopen(info->dlpi_name, RTLD_NOLOAD);
+            dlclose(h);
+            if (h == mc) {
+                for (auto& phdr : std::span{info->dlpi_phdr, info->dlpi_phnum}) {
+                    if (phdr.p_type == PT_LOAD && phdr.p_flags & PF_X) {
+                        r = {reinterpret_cast<std::byte*>(info->dlpi_addr + phdr.p_vaddr), phdr.p_memsz};
+                        return 1;
+                    }
                 }
             }
-        }
-        return 0;
-    }, dlopen("libminecraftpe.so", 0));
+            return 0;
+        },
+        dlopen("libminecraftpe.so", 0));
 
-    {
-        constexpr auto sig  = "55 41 57 41 56 53 48 83 EC ? 41 89 CF 49 89 D6 48 89 FB 64 48 8B 04 25 28 00 00 00 48 89 44 24 ? 48 8B 7E"_sig;
-        auto           addr = hat::find_pattern<hat::scan_alignment::X16>(r.begin(), r.end(), sig).get();
-        ResourcePackManager_ctor_hook = safetyhook::create_inline(addr, reinterpret_cast<void*>(ResourcePackManager_ctor));
-    }
+    auto ResourcePackManager_ctor_addr = hat::find_pattern<hat::scan_alignment::X16>(
+        r.begin(), r.end(),
+        "55 41 57 41 56 53 48 83 EC ? 41 89 CF 49 89 D6 48 89 FB 64 48 8B 04 25 28 00 00 00 48 89 44 24 ? 48 8B 7E"_sig);
 
-    {
-        constexpr auto sig  = "41 57 41 56 41 54 53 48 81 EC ? ? ? ? 49 89 FE 64 48 8B 04 25 28 00 00 00 48 89 84 24 ? ? ? ? 0F 57 C0 0F 29 44 24 ? 48 8D BC 24"_sig;
-        auto           addr = hat::find_pattern<hat::scan_alignment::X16>(r.begin(), r.end(), sig).get();
-        AppPlatform_readAssetFile_hook = safetyhook::create_inline(addr, reinterpret_cast<void*>(AppPlatform_readAssetFile));
-    }
+    ResourcePackManager_ctor_hook = safetyhook::create_inline(ResourcePackManager_ctor_addr.get(), reinterpret_cast<void*>(ResourcePackManager_ctor));
+
+    auto AppPlatform_readAssetFile_addr = hat::find_pattern<hat::scan_alignment::X16>(
+        r.begin(), r.end(),
+        "41 57 41 56 41 54 53 48 81 EC ? ? ? ? 49 89 FE 64 48 8B 04 25 28 00 00 00 48 89 84 24 ? ? ? ? 0F 57 C0 0F 29 44 24 ? 48 8D BC 24"_sig);
+
+    AppPlatform_readAssetFile_hook = safetyhook::create_inline(AppPlatform_readAssetFile_addr.get(), reinterpret_cast<void*>(AppPlatform_readAssetFile));
 }
